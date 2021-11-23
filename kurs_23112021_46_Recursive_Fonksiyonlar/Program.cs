398e67d66ae912e5ac4eef9ee52e58dd2aaf650d
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-23 18:35:40 +0300

    Add project files.

---
+++ kurs_23112021_46_Recursive_Fonksiyonlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_23112021_46_Recursive_Fonksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                long sayiFaktoriyel = faktoriyel(i);
+                Console.WriteLine("{0} sayısının faktöriyeli: {1}",i,sayiFaktoriyel);
+            }
+            Console.ReadLine();
+        }
+        static long faktoriyel(long sayac)
+        {
+            if (sayac<=1)
+            {
+                return 1;
+            }
+            return sayac * faktoriyel(sayac - 1);
+        }
+    }
+}
